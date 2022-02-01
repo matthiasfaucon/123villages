27a6d5a508de5e5e00c246048b9305e8b8467f07
matthias faucon <matthias.faucon@orange.fr>
2022-02-01 15:34:02 +0100

    Création page département

---
+++ departement.php
@@ -0,0 +1,29 @@
+<!-- <?php
+
+    include 'php/verif_script.php';
+
+?> -->
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+  <!-- <script src="script.js"></script> -->
+</head>
+<body>
+
+<header>
+    <p id="logoTypo">MonVillageNormand.fr</p>
+    <div id="searchBarre"><p>Rechercher un village...</p><a href="departement.html"><div id="research"></div></a></div>
+    <div id="banner"><img src="images/banniere_deskstop.jpg"></div>
+</header>
+
+
+</body>
+
+</html>

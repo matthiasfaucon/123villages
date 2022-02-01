4048254710d0ba73a81d7dcedfde6cfac32e972e
matthias faucon <matthias.faucon@orange.fr>
2022-02-01 14:26:32 +0100

    ajout du package API

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta charset="utf-8">
   <title>Titre de la page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="css/style.css">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
   <!-- <script src="script.js"></script> -->

846963f132dcab38128d578fde8d9bd2db5ddced
Flamme <lisa.flamme@univ-rouen.fr>
2022-02-01 15:53:38 +0100

    créa fichier services.php

---
+++ php/services.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<head>
+    
+    <title>Services</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+    <header id="header-services">
+            <img id="banniere-image" src="../images/banniere.jpg" alt=""/>
+            <h1 id="titre-du-site"><a href="../index.php">MonVillageNormand.fr</a></h1>
+            <div id="barre-de-recherche">
+                <input type="text" id="zone-de-recherche" name="zone-de-recherche">
+                <a href=""><img id="loupe-de-recherche" src="../images/loupe.png" alt="bouton rechercher"/></a>
+            </div>
+            <button id="connexion"> Se connecter </button>
+    </header>
+
+    <div id="fil-ariane"> 
+        <ul id="fil-ariane-historique">
+            <li class="element-fil-ariane">Elbeuf</li>
+            <li class="element-fil-ariane">76500</li>
+            <li class="element-fil-ariane">Village fleuri</li>
+        </ul>
+    </div>
+
+    <div id="categorie">
+        <p> Logo service </p>
+    </div>
+
+    <div class="div-principal">
+        <h2 class="titre-div-principal h2-principaux">Présentation</h2>
+        <p>Quelques informations par rapport à la ville (voir BDD)</p>
+    </div>
+    <hr />
+    <div class="div-principal">
+        <h2 class="titre-div-principal h2-principaux">Les différents services</h2>
+        <p>CARTE</p>
+    </div>
+    <hr />
+    <div class="div-principal">
+        <h2 class="titre-div-principal">Associations</h2>
+        <p>CARTE</p>
+    </div>
+
+    <div class="div-principal">
+        <h2 class="titre-div-principal">Transports</h2>
+        <p>CARTE</p>
+    </div>
+    
+    <div class="div-principal">
+        <h2 class="titre-div-principal">Santé</h2>
+        <p>CARTE</p>
+    </div>
+
+    <footer>
+        <a href="">Mentions légales et partenaires</a>
+        <p> Mis à jour le : </p>
+    </footer>
+</body>
+</html>

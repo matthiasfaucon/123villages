e782a2f7797a07d374fb533857366299f0f49cb7
leo <cavarecleo@gmail.com>
2022-02-22 18:03:28 +0100

    Ajout du header

---
+++ php/ville.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+  <!-- <script src="script.js"></script> -->
+</head>
+<body>
+
+<header>
+    <p id="logoTypo">MonVillageNormand.fr</p>
+    <div id="searchBarre"><p>Rechercher un village...</p><a href="departement.html"><div id="research"></div></a></div>
+    <div id="banner"><img src="images/banniere_deskstop.jpg"></div>
+</header> 

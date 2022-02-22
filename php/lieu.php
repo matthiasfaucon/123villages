81db8ce599e7bfc5dacec6f8c93da2e8a36dfeda
leo <cavarecleo@gmail.com>
2022-02-22 17:15:44 +0100

    Ajout carte

--- php/lieu.php
+++ php/lieu.php
@@ -41,4 +41,49 @@
         <p>Coordonnées GPS: 49.1738014221, 0.7868893743</p>
         
     </div>
- </div>
+ </div> 
+ <div id="maCarte1"> </div> 
+    <!-- Fichiers Javascript -->
+<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+<script>
+    // On initialise la carte
+    var carte = L.map('maCarte1').setView([48.852969, 2.349903], 13);
+    
+    // On charge les "tuiles"
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(carte);
+
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = () => {
+        // La transaction est terminée ?
+        if(xmlhttp.readyState == 4){
+            // Si la transaction est un succès
+            if(xmlhttp.status == 200){
+                // On traite les données reçues
+                let donnees = JSON.parse(xmlhttp.responseText)
+                
+                // On boucle sur les données (ES8)
+                Object.entries(donnees.agences).forEach(agence => {
+                    // Ici j'ai une seule agence
+                    // On crée un marqueur pour l'agence
+                    let marker = L.marker([agence[1].lat, agence[1].lon]).addTo(carte)
+                    marker.bindPopup(agence[1].nom)
+                })
+            }else{
+                console.log(xmlhttp.statusText);
+            }
+        }
+    }
+
+    xmlhttp.open("GET", "agence/liste_simple.php");
+
+    xmlhttp.send(null);
+</script>
+
+</div> 
+</div>

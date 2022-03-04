227914cca456236f86bde0ed62dc186929d0ecf1
leo_cavarec <cavarecleo@gmail.com>
2022-03-04 22:35:06 +0100

    Ajout fleche (a modifier)

---
+++ php/front_connexion.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <div id="arrow">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="20-Arrow Left"> 
+    <path d="m8.41 15 5.29-5.29-1.41-1.42-7 7a1 1 0 0 0 0 1.41l7 7 1.41-1.41L8.41 17H27v-2z"/></g></svg>
+    </div>
+
+    <div id="peopleandcracc">
+    <div id="people"></div>
+    <div id="cracc"></div>
+
+    </div>
+
+
+</body>
+</html>

2ca4f551541f17aef7255c3da1d0b291ffc8a637
leo_cavarec <cavarecleo@gmail.com>
2022-03-06 02:14:39 +0100

    creation fichier front_create_account

---
+++ php/front_create_account.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <div id="arrow">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+            <g data-name="20-Arrow Left">
+                <path d="m8.41 15 5.29-5.29-1.41-1.42-7 7a1 1 0 0 0 0 1.41l7 7 1.41-1.41L8.41 17H27v-2z" />
+            </g>
+        </svg>
+    </div>
+
+
+    <div id="peopleandcracc">
+        <div id="people"> </div>
+        <div id="cracc">
+            <div id="lcoin">
+                <h4>INSCRIPTION</h4>
+                <a href="front_inscription.php">Déjà un compte ? </a>
+            </div>
+
+            <div id="apigf">
+                <div id="apig">
+                    <p>S'inscrire avec Google</p>
+                </div>
+                <div id="apif">
+                    <p>S'inscrire avec Facebook</p>
+                </div>
+            </div>
+
+            <form action="" method="get" class="form_frontco_form">
+
+                <div class="form_frontco">
+                    <label for="email"> </label>
+                    <input type="email" name="email" id="email" placeholder="Adresse email" required>
+                </div>
+
+                <div class="form_frontco">
+                    <label for="password"></label>
+                    <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+                </div>
+
+                <div class="form_frontco_button">
+                    <input type="submit" value="Envoyer">
+                </div>
+            </form>
+
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>

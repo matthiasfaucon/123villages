4cb1bb12074bc2de73fda5b74bc371260b90b2ec
leo_cavarec <cavarecleo@gmail.com>
2022-03-10 22:38:26 +0100

    creation menu page admin

---
+++ php/admin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=*, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" type="image/x-icon" href="../images/favicon.svg">
+    <title>Gestion de contenue</title>
+</head>
+<body> 
+
+<div id="menu_gestion_admin">
+
+<p>Membre</p>
+<ul>
+
+<li>Dépôt de photos</li> 
+<li>Proposition modification d'informations</li>
+<li>Photos validées</li> 
+
+</ul> 
+<p>Admin</p>
+
+<ul>
+
+<li>Espace validation photos</li> 
+<li>Espace validation texte</li> 
+<li>Espace création lieu</li> 
+<li>Campagnes de mail</li> 
+<li>mise à jour SiteMap</li> 
+<li>Indexation</li> 
+<li>supervision du département </li> 
+</ul> 
+<p>Admin département </p>
+<ul>
+<li>Gestion Admin Département</li>
+
+</ul>
+
+</div>
+
+    
+</body>
+</html>

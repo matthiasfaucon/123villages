bdec8f3cf967376ccaa790becab35b780073b57c
leo_cavarec <cavarecleo@gmail.com>
2022-03-10 23:11:50 +0100

    putain de menu

--- php/admin.php
+++ php/admin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,38 +9,50 @@
     <link rel="icon" type="image/x-icon" href="../images/favicon.svg">
     <title>Gestion de contenue</title>
 </head>
-<body> 
 
-<div id="menu_gestion_admin">
+<body>
 
-<p>Membre</p>
-<ul>
+    <div id="gestion_admin">
 
-<li>Dépôt de photos</li> 
-<li>Proposition modification d'informations</li>
-<li>Photos validées</li> 
+        <div>
+            <p>Membre</p>
 
-</ul> 
-<p>Admin</p>
+            <nav class="menu_gestion_membre">
+                <a href="#" class="menu__item_gestion_membre">Dépôt de photos</a>
+                <a href="#" class="menu__item_gestion_membre">Proposition modification d'informations</a>
+                <a href="#" class="menu__item_gestion_membre">Photos validées</a>
+            </nav>
+        </div>
+        <div>
+            <p>Admin</p>
 
-<ul>
 
-<li>Espace validation photos</li> 
-<li>Espace validation texte</li> 
-<li>Espace création lieu</li> 
-<li>Campagnes de mail</li> 
-<li>mise à jour SiteMap</li> 
-<li>Indexation</li> 
-<li>supervision du département </li> 
-</ul> 
-<p>Admin département </p>
-<ul>
-<li>Gestion Admin Département</li>
+            <nav class="menu_gestion_admin">
+                <a href="#" class="menu__item_gestion_admin">Espace validation photos</a></br>
+                <a href="#" class="menu__item_gestion_admin">Espace validation texte</a></br>
+                <a href="#" class="menu__item_gestion_admin">Espace création lieu</a> </br>
+                <a href="#" class="menu__item_gestion_admin">Campagnes de mail</a></br>
+                <a href="#" class="menu__item_gestion_admin">mise à jour SiteMap</a></br>
+                <a href="#" class="menu__item_gestion_admin">Indexation</a> </br>
+                <a href="#" class="menu__item_gestion_admin">supervision du département</a></br>
+            </nav>
+        </div>
+        <div>
 
-</ul>
+            <p>Admin département </p>
 
-</div>
+            <nav class="menu_gestion_admin_dep">
 
-    
+                <a href="#" class="menu__item_gestion_admin">Gestion Admin Département</a></br>
+
+            </nav>
+        </div>
+    </div>
+
+
+
+
+
 </body>
+
 </html>

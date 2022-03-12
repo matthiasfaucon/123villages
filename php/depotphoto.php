0951ec0cd13f18ac4066940bab8152c8f53965a1
leo_cavarec <cavarecleo@gmail.com>
2022-03-12 20:10:44 +0100

    creation page depot de photo

---
+++ php/depotphoto.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>depot_photo</title>
+</head>
+<body>
+<div id="gestion_admin">
+
+<div id="membre">
+    <p>Membre</p>
+
+    
+        <span class="menu__item_gestion_membre">Dépôt de photos</span></br>
+        <span class="menu__item_gestion_membre">Proposition modification d'informations</span></br>
+        <span class="menu__item_gestion_membre">Photos validées</span></br>
+    
+</div>
+ <div id="admin"> 
+    <p>Admin</p>
+
+
+   
+         <span class="menu__item_gestion_admin">Espace validation photos</span></br>
+         <span class="menu__item_gestion_admin">Espace validation texte</span></br>
+         <span class="menu__item_gestion_admin">Espace création lieu</span> </br>
+         <span class="menu__item_gestion_admin">Campagnes de mail</span></br>
+         <span class="menu__item_gestion_admin">mise à jour SiteMap</span></br>
+         <span class="menu__item_gestion_admin">Indexation</span> </br>
+         <span class="menu__item_gestion_admin">supervision du département</span></br>
+ 
+</div> 
+
+<div id="admindep">
+
+    <p>Admin département </p>
+    <span class="menu__item_gestion_admindep">Gestion Admin Département</span>
+</div> 
+</div>
+
+
+
+    
+</body>
+</html>

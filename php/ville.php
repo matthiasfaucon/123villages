5c716a9c0d01d3b68c8937f2dd3fa1e205cbe0a7
leo_cavarec <cavarecleo@gmail.com>
2022-03-13 22:52:12 +0100

    fin ville

--- php/ville.php
+++ php/ville.php
@@ -39,8 +39,10 @@
       <p>76500</p>
     </div>
     <div id="village_fleuri">
-      <div><p>village fleuri</p>
-            <img src=""></div>
+      <div>
+        <p>village fleuri</p>
+        <img src="">
+      </div>
     </div>
   </section>
   <section id="presentation">
@@ -52,7 +54,149 @@
       </div>
     </div>
   </section>
-  <br><br><br><br><br><br><br><br>
+  <section id="activites-ville">
+    <h1>Activités à Elbeuf</h1>
+    <div id="menuSecondaireHorizontal">
+      <span class="menu-link">Services</span>
+      <span class="menu-link">Patrimoine</span>
+      <span class="menu-link">Commerces</span>
+      <span class="menu-link">Découvrir</span>
+
+    </div>
+    <div class="carteInformations">
+      <div id="maCarteDepartement"></div>
+      <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+      <script>
+        // On initialise la carte
+        var carte = L.map('maCarteDepartement').setView([49.290628552849746, 1.0112003960901552], 13);
+
+        // On charge les "tuiles"
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+          // Il est toujours bien de laisser le lien vers la source des données
+          attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+          minZoom: 1,
+          maxZoom: 20
+        }).addTo(carte);
+
+        let xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = () => {
+          // La transaction est terminée ?
+          if (xmlhttp.readyState == 4) {
+            // Si la transaction est un succès
+            if (xmlhttp.status == 200) {
+              // On traite les données reçues
+              let donnees = JSON.parse(xmlhttp.responseText)
+
+              // On boucle sur les données (ES8)
+              Object.entries(donnees.agences).forEach(agence => {
+                // Ici j'ai une seule agence
+                // On crée un marqueur pour l'agence
+                let marker = L.marker([agence[1].lat, agence[1].lon]).addTo(carte)
+                marker.bindPopup(agence[1].nom)
+              })
+            } else {
+              console.log(xmlhttp.statusText);
+            }
+          }
+        }
+
+        xmlhttp.open("GET", "agence/liste_simple.php");
+
+        xmlhttp.send(null);
+      </script>
+    </div> 
+  </section>
+
+    <section id="presentationCarte">
+    <h1>Notre sélection pour la Seine-Marritime</h1>
+    <div class="contenerCard">
+    <div class="cards">
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>../
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+
+      <div class="card">
+        <div class="card-background" style="background-image: url(../images/batiment-avec-fond/avoir/eglisecatholique.jpg);"></div>
+          <div class="ville">
+            <h3>Elbeuf</h3>
+          </div>
+          <div class="monument">
+            <p>Eglise d'Elbeuf</p>
+          </div>
+      </div>
+</div>
+        
+</section> 
+
+
+  </section>
   <footer>Mentions légales</footer>
 </body>
 

70608eb400ddcfb5cbcdf78649237ac7123b9129
Faucon Matthias <matthias.faucon@orange.fr>
2022-03-14 19:22:37 +0100

    ajout menu mobile

---
+++ php/codePostale.php
@@ -0,0 +1,89 @@
+<!-- <?php
+
+include 'php/verif_script.php';
+
+?> -->
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Departement</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/style.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" type="image/x-icon" href="../images/favicon.svg">
+  <!-- <script src="script.js"></script> -->
+  <script src="https://kit.fontawesome.com/7eb76eff39.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<header>
+    <p id="logoTypo">LaVieDuVillage.fr</p>
+    <div id="searchBarre"><p>Rechercher un villwage...</p><a href="departement.php"><div id="research"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M236 176c0 15.46-12.54 28-28 28S180 191.5 180 176S192.5 148 208 148S236 160.5 236 176zM500.3 500.3c-15.62 15.62-40.95 15.62-56.57 0l-119.7-119.7c-40.41 27.22-90.9 40.65-144.7 33.46c-91.55-12.23-166-87.28-177.6-178.9c-17.24-136.2 97.29-250.7 233.4-233.4c91.64 11.6 166.7 86.07 178.9 177.6c7.19 53.8-6.236 104.3-33.46 144.7l119.7 119.7C515.9 459.3 515.9 484.7 500.3 500.3zM294.1 182.2C294.1 134.5 255.6 96 207.1 96C160.4 96 121.9 134.5 121.9 182.2c0 38.35 56.29 108.5 77.87 134C201.8 318.5 204.7 320 207.1 320c3.207 0 6.26-1.459 8.303-3.791C237.8 290.7 294.1 220.5 294.1 182.2z"/></svg></div></a></div>
+    <div id="banner"><img src="../images/banniere.jpg"/></div>
+</header>
+
+<div id="apparitionMenu">
+        <div id="toggle"></div>
+        
+        
+    <div id="menuToggleDiv">
+            <p><a class="menuToggle" href="#prenom">Accueil</a></p>
+            <p><a class="menuToggle" href="#presentation">Présentation</a></p>
+            <p><a class="menuToggle" href="#services">Services</a></p>
+            <p><a class="menuToggle" href="#creations">Créations</a></p>
+            <p><a class="menuToggle" href="#contact">Contact</a></p>
+    </div>
+    </div>
+
+    <script type="text/javascript" src="../js/menu.js"></script>
+
+<section id="codePostale">
+<div class="76000">
+<h2>Code Postal 76000</h2>
+<a>Rouen</a>
+</div>
+
+<div class="76110">
+<h2>Code Postal 76110</h2>
+<a>Angerville-Bailleul 
+|  Annouville-Vilmesnil
+|  Auberville-la-Renault
+Bec-de-Mortagne  |  Bénarville  |  Bornambusc  |  Bréaut Bretteville-du-Grand-Caux  |  Daubeuf-Serville  |  Écrainville
+Goderville  |  Gonfreville-Caillot  |  Grainville-Ymauville   Houquetot  |  Manneville-la-Goupil  |  Mentheville  |  Saint-Maclou-la-Brière  |  Saint-Sauveur-d'Émalleville  |  Sausseuzemare-en-Caux  |  Tocqueville-les-Murs  |  Vattetot-sous-Beaumont  |  Virville</a>
+</div>
+
+<div class="76111">
+<h2>Code Postal 76111</h2>
+<a>Criquebeuf-en-Caux  |  Vattetot-sur-Mer  |  Yport</a>
+</div>
+
+<div class="76113">
+<h2>Code Postal 76113</h2>
+<a>Hautot-sur-Seine  |  Sahurs  |  Saint-Pierre-de-Manneville</a>
+</div>
+
+<div class="76117">
+<h2>Code Postal 76117</h2>
+<a>Auzouville-sur-Ry  |  Blainville-Crevon  |  Catenay  |  Grainville-sur-Ry  |  Martainville-Épreville  |  Ry  |  Saint-Aignan-sur-Ry  |  Saint-Denis-le-Thiboult  |  Servaville-Salmonville</a>
+</div>
+
+<div class="76116">
+<h2>Code Postal 76116</h2>
+<a>Incheville
+Code Postal 76119
+Sainte-Marguerite-sur-Mer  |  Varengeville-sur-Mer
+Code Postal 76120
+Le Grand-Quevilly
+Code Postal 76130
+	Mont-Saint-Aignan
+Code Postal 76133
+Épouville  |  Manéglise  |  Notre-Dame-du-Bec  |  Rolleville  |  Saint-Martin-du-Bec</a>
+</div>
+</section>
+
+<footer>Mentions légales</footer>
+</body>
+
+</html>

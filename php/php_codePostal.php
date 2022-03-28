2eb60fd77a9a7654ad9e99fc94338ab07720ed91
Bacup83 <pro.bacup@gmail.Com>
2022-03-28 10:52:32 +0200

    mise à jour verif_script.php

--- php/php_codePostal.php
+++ php/php_codePostal.php
@@ -1,22 +1,25 @@
 <?php
 
-try
-    {
+try{
         $bdd=new PDO('mysql:host=localhost;dbname=ptut','root','');
     }
-    catch(Exception $e)
-    {
+    catch(Exception $e){
         die('Erreur :'.$e->getMessage());
     }
 
     $reponse=$bdd->query('SELECT code_postal, nom_ville FROM code_postaux');
 
-    while ($donnees=$reponse->fetch())
-    {
+    while ($donnees=$reponse->fetch()){
+    
       $codePostal = $donnees['code_postal'];
       $ville = $donnees['nom_ville'];
 
       $text = $donnees['nom_ville'];
       echo $text;
+
+    }
+    
+
+    $reponse->closeCursor();
 
       ?>

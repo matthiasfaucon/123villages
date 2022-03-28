66e9c068829eea3659b5582317cb359d5d09dde4
Bacup83 <pro.bacup@gmail.Com>
2022-03-28 05:11:57 +0200

    Dynamisme du bouton de compte sur index

--- index.php
+++ index.php
@@ -31,7 +31,21 @@
 <img src="images/banniere2.png"/>
       <p id="logoTypo">LaVieDuVillage.fr</p>
     <div id="profil"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"/></svg></div>
-    <div id="menuProfil"><a>Mon compte</a><a href="php/deconexion_back.php">se déconnecter</a></div>
+    
+    <?php
+
+      if($_SESSION['verif'] == 1){
+        ?>
+          <div id="menuProfil"><a>Mon compte</a><a href="php/deconexion_back.php">se déconnecter</a></div>
+        <?php
+      }else{
+        ?>
+        <div id="menuProfil"><a href="php/front_connexion.php">Connexion</a><a href="php/front_create_account.php">Inscription</a></div>
+        <?php
+      }
+
+    ?>
+
     <div id="searchBarre">
     <form action="php/searchleo.php">
       <input type="text" id="name" name="name" required placeholder="Rechercher...">

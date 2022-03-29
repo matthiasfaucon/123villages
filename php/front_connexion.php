582460eb75ce9efc440e3e021fd2e1a67e1b527c
Bacup83 <pro.bacup@gmail.Com>
2022-03-29 11:51:46 +0200

    indentation page de co/inscr

--- php/front_connexion.php
+++ php/front_connexion.php
@@ -11,107 +11,118 @@
     <title>Connexion</title>
 
     <meta property="og:title" content="Se connecter, c'est partager" />
-  <meta property="og:type" content="article" />
-  <meta property="og:description" content="Une page de connexion, vous permettant de paratger vos plus belles photos pour nourir le partage du patrimoine " />
-  <meta property="og:url" content="http://localhost/mmi2/123villages/" />
-  <meta property="og:image" content="images/openGraph/laVieDuVillage.png" />
-  <meta property="og:site_name" content="Site 123villages" />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="Une page de connexion, vous permettant de paratger vos plus belles photos pour nourir le partage du patrimoine " />
+    <meta property="og:url" content="http://localhost/mmi2/123villages/" />
+    <meta property="og:image" content="images/openGraph/laVieDuVillage.png" />
+    <meta property="og:site_name" content="Site 123villages" />
 
     <?php
 
-session_start();
+    session_start();
 
-if(!isset($_SESSION['email_verif']) || !isset($_SESSION['mdp_verif'])){
-    $_SESSION['email_verif'] = '';
-    $_SESSION['mdp_verif'] = '';
-}
+    if (!isset($_SESSION['email_verif']) || !isset($_SESSION['mdp_verif'])) {
+        $_SESSION['email_verif'] = '';
+        $_SESSION['mdp_verif'] = '';
+    }
 
-if(!isset($_SESSION['email_exist'])){
-    $_SESSION['email_exist'] = '';
-}
+    if (!isset($_SESSION['email_exist'])) {
+        $_SESSION['email_exist'] = '';
+    }
 
-if($_SESSION['email_verif'] == 1 && $_SESSION['mdp_verif'] == 1){
-    header('Location: ../index.php');
-}
+    if ($_SESSION['email_verif'] == 1 && $_SESSION['mdp_verif'] == 1) {
+        header('Location: ../index.php');
+    }
 
-if(!isset($_SESSION['no_email'])){
-    $_SESSION['no_email'] = '';
-}
+    if (!isset($_SESSION['no_email'])) {
+        $_SESSION['no_email'] = '';
+    }
 
-if(!isset($_SESSION['verif'])){
-    $_SESSION['verif'] = 0;
-}
+    if (!isset($_SESSION['verif'])) {
+        $_SESSION['verif'] = 0;
+    }
 
-if($_SESSION['verif'] == 1){
-    header('Location: ../index.php');
-}
+    if ($_SESSION['verif'] == 1) {
+        header('Location: ../index.php');
+    }
 
-?>
+    ?>
 
 </head>
 
 <body>
-  
 
 
 
-<div id="arrow">
-<svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 314.33"><defs><style>.cls-1{fill:#4062bb;}</style></defs><title>up-arrow-svgrepo-com</title><polygon class="cls-1" points="31.93 164.82 490 164.82 490 149.51 31.93 149.51 189.08 11.52 178.97 0 0 157.17 178.97 314.33 189.08 302.82 31.93 164.82"/></svg>
-</svg>
+
+    <div id="arrow">
+        <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 314.33">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #4062bb;
+                    }
+                </style>
+            </defs>
+            <title>up-arrow-svgrepo-com</title>
+            <polygon class="cls-1" points="31.93 164.82 490 164.82 490 149.51 31.93 149.51 189.08 11.52 178.97 0 0 157.17 178.97 314.33 189.08 302.82 31.93 164.82" />
+        </svg>
+        </svg>
 
     </div>
     <div class="seConnecter">
-    <div id="peopleandcracc">
-        <div id="people"> </div>
-        <div id="cracc">
-            <div id="lcoin">
-                <h4>CONNEXION</h4>
-                <a href="front_create_account.php">Pas encore de compte ? </a>
-            </div>
-
-            <div id="apigf">
-                <div id="apig">
-                    <img src="../images/google.png"/><p>Connexion avec Google</p>
-                </div>
-                <div id="apif">
-                    <p>Connexion avec Facebook</p>
-                </div>
-            </div>
-
-            <form action="connexion_back.php" method="post" class="form_frontco_form">
-
-                <div class="form_frontco">
-                    <label for="email"> </label>
-                    <input type="email" name="email" id="email" placeholder="Adresse email" required>
+        <div id="peopleandcracc">
+            <div id="people"> </div>
+            <div id="cracc">
+                <div id="lcoin">
+                    <h4>CONNEXION</h4>
+                    <a href="front_create_account.php">Pas encore de compte ? </a>
                 </div>
 
-                <?php
-                    if($_SESSION['no_email'] == 1){
+                <div id="apigf">
+                    <div id="apig">
+                        <img src="../images/google.png" />
+                        <p>Connexion avec Google</p>
+                    </div>
+                    <div id="apif">
+                        <p>Connexion avec Facebook</p>
+                    </div>
+                </div>
+
+                <form action="connexion_back.php" method="post" class="form_frontco_form">
+
+                    <div class="form_frontco">
+                        <label for="email"> </label>
+                        <input type="email" name="email" id="email" placeholder="Adresse email" required>
+                    </div>
+
+                    <?php
+                    if ($_SESSION['no_email'] == 1) {
                         echo '<p>Veuillez entrer une adresse email valide</p><br>';
                     }
-                ?>
+                    ?>
 
-                <div class="form_frontco">
-                    <label for="password"></label>
-                    <input type="password" name="password" id="password" placeholder="Mot de passe" required>
-                </div>
+                    <div class="form_frontco">
+                        <label for="password"></label>
+                        <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+                    </div>
 
-                <?php
-                    if($_SESSION['email_verif'] == 0 || $_SESSION['mdp_verif'] == 0){
+                    <?php
+                    if ($_SESSION['email_verif'] == 0 || $_SESSION['mdp_verif'] == 0) {
                         echo '<p>adresse e-mail ou mot de passe incorrect</p><br>';
                     }
 
-                ?>
+                    ?>
 
-                <div class="form_frontco_button">
-                    <input type="submit" value="Envoyer">
-                </div>
-            </form>
+                    <div class="form_frontco_button">
+                        <input type="submit" value="Envoyer">
+                    </div>
+                </form>
 
 
+            </div>
         </div>
     </div>
-</div>
 
 </body>
 
--- php/front_create_account.php
+++ php/front_create_account.php
@@ -9,37 +9,47 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="icon" type="image/x-icon" href="../images/favicon.svg">
     <title>Inscription</title>
-    	
+
     <meta property="og:title" content="Rejoignez et soutenez-nous dans la découverte du patrimoine français" />
-  <meta property="og:type" content="article" />
-  <meta property="og:description" content="Une page d'inscription, vous permettant de vous créez un compte et de contribuer au partage du patrimoine " />
-  <meta property="og:url" content="http://localhost/mmi2/123villages/" />
-  <meta property="og:image" content="images/openGraph/laVieDuVillage.png" />
-  <meta property="og:site_name" content="Site 123villages" />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="Une page d'inscription, vous permettant de vous créez un compte et de contribuer au partage du patrimoine " />
+    <meta property="og:url" content="http://localhost/mmi2/123villages/" />
+    <meta property="og:image" content="images/openGraph/laVieDuVillage.png" />
+    <meta property="og:site_name" content="Site 123villages" />
     <?php
 
-session_start();
+    session_start();
 
-if(!isset($_SESSION['email_exist'])){
-    $_SESSION['email_exist'] = '';
-}
+    if (!isset($_SESSION['email_exist'])) {
+        $_SESSION['email_exist'] = '';
+    }
 
-if(!isset($_SESSION['no_email'])){
-    $_SESSION['no_email'] = '';
-}
+    if (!isset($_SESSION['no_email'])) {
+        $_SESSION['no_email'] = '';
+    }
 
-if($_SESSION['verif'] == 1){
-    header('Location: ../index.php');
-}
+    if ($_SESSION['verif'] == 1) {
+        header('Location: ../index.php');
+    }
 
-?>
+    ?>
 
 </head>
 
 <body>
     <div id="arrow">
-    <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 314.33"><defs><style>.cls-1{fill:#4062bb;}</style></defs><title>up-arrow-svgrepo-com</title><polygon class="cls-1" points="31.93 164.82 490 164.82 490 149.51 31.93 149.51 189.08 11.52 178.97 0 0 157.17 178.97 314.33 189.08 302.82 31.93 164.82"/></svg>
-</svg>
+        <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 314.33">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #4062bb;
+                    }
+                </style>
+            </defs>
+            <title>up-arrow-svgrepo-com</title>
+            <polygon class="cls-1" points="31.93 164.82 490 164.82 490 149.51 31.93 149.51 189.08 11.52 178.97 0 0 157.17 178.97 314.33 189.08 302.82 31.93 164.82" />
+        </svg>
+        </svg>
     </div>
 
 
@@ -62,7 +72,7 @@ if($_SESSION['verif'] == 1){
 
             <form action="inscription_back.php" method="post" class="form_frontco_form">
 
-            <div class="form_frontco">
+                <div class="form_frontco">
                     <label for="name"> </label>
                     <input type="text" name="prenom" id="name" placeholder="Prénom" required>
                 </div>
@@ -70,7 +80,7 @@ if($_SESSION['verif'] == 1){
                 <div class="form_frontco">
                     <label for="fname"> </label>
                     <input type="text" name="nom" id="fname" placeholder="Nom" required>
-                </div>             
+                </div>
 
                 <div class="form_frontco">
                     <label for="email"> </label>
@@ -83,13 +93,13 @@ if($_SESSION['verif'] == 1){
                 </div>
 
                 <?php
-                    if($_SESSION['email_exist'] == 1){
-                        echo '<p>adresse e-mail déjà existant</p><br>';
-                    }
+                if ($_SESSION['email_exist'] == 1) {
+                    echo '<p>adresse e-mail déjà existant</p><br>';
+                }
 
-                    if($_SESSION['no_email'] == 1){
-                        echo '<p>Veuillez entrer une adresse email valide</p><br>';
-                    }
+                if ($_SESSION['no_email'] == 1) {
+                    echo '<p>Veuillez entrer une adresse email valide</p><br>';
+                }
                 ?>
 
                 <div class="form_frontco_button">
